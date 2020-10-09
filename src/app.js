e053c346608a6133cfcd60cb6b82903d411e2efc
Felipe Nuñez <toutox@gmail.com>
2020-10-09 10:46:00 -0300

    fix(app);fix api /contract/:id

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { Op } = require('sequelize');
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { params: { id }, profile } = req;
+    const { Contract } = req.app.get('models');
+    const contract = await Contract.findOne({
+        where: {
+            id: id,
+            [Op.or]: [
+                { ClientId: profile.id },
+                { ContractorId: profile.id }
+            ]
+        }
+    })
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
 module.exports = app;
